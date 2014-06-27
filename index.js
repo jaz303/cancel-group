17af77746df6c3330ab0a81db6d6bc2f14ca8507
Jason Frame <jason@onehackoranother.com>
2014-06-27 15:26:18 +0100

    tabs -> spaces

--- index.js
+++ index.js
@@ -2,25 +2,25 @@ module.exports = cancelGroup;
 
 function cancelGroup() {
 
-	var cancelled = false;
-	var callbacks = [];
+    var cancelled = false;
+    var callbacks = [];
 
-	return function(cb) {
+    return function(cb) {
 
-		if (cancelled) {
-			if (cb) cb();
-			return;
-		}
+        if (cancelled) {
+            if (cb) cb();
+            return;
+        }
 
-		if (cb === void 0) {
-			cancelled = true;
-			for (var i = 0, l = callbacks.length; i < l; ++i) {
-				callbacks[i]();
-			}
-		} else {
-			callbacks.push(cb);
-		}
+        if (cb === void 0) {
+            cancelled = true;
+            for (var i = 0, l = callbacks.length; i < l; ++i) {
+                callbacks[i]();
+            }
+        } else {
+            callbacks.push(cb);
+        }
 
-	};
+    };
 
 }
